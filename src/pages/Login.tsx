ad4057f84a8b74e5152d35b7f2e37d23819e51ac
Moses Mwangi <ngigemoses1@outlook.com>
2023-05-04 23:46:11 +0300

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import {Form} from 'react-router-dom';
+
 
 export default function Login(): JSX.Element {
+
+
+
 	return (
 		<>
-			<h1>Login Page</h1>
+			<div className="container">
+				<Form method='post'>
+					<div className="mb-3">
+						<label htmlFor="email" className="form-label">Email address</label>
+						<input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+						<div id="emailHelp" className="form-text"></div>
+
+						<div className="mb-3">
+							<label htmlFor="password" className="form-label">Password</label>
+							<input type="password" className="form-control" id="password"/>
+						</div>
+
+						{/* TODO: Add grid row */}
+						<button id="loginButton" name="loginButton" type="submit" className="btn btn-primary" value="login">Login</button>
+						<button id="registerButton" name="registerButton" type="submit" className="btn btn-success" value="register">Register</button>
+
+						{/* TODO: Add forget option
+						<a href="dash.html">dash</a> */}
+					</div>
+				</Form>
+			</div>
 		</>
 	);
 }
