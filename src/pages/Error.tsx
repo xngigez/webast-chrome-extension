d523920a26b6ad94e882b9e6f8e0b7bd1a489883
Moses Mwangi <ngigemoses1@outlook.com>
2023-05-04 23:46:27 +0300

    Update Error.tsx

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,9 +1,15 @@
+import { useRouteError, Link } from 'react-router-dom';
+
 export default function Error(): JSX.Element {
+	const error:any = useRouteError();
+
 	return (
 		<>
 			<h1>You got lost!</h1>
 			<p>
-				<a href='/'>Go to home</a>
+				<i>Issue: {error.statusText || error.message}</i>
+				<br/>
+				<Link to='/'>Go to home</Link>
 			</p>
 		</>
 	);
