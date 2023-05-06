b16d3d40ca52c4138b324c2d9689e9cf1084d55c
Moses Mwangi <ngigemoses1@outlook.com>
2023-05-06 22:29:03 +0300

    Remove throw error for null token

--- src/services/auth/AuthToken.tsx
+++ src/services/auth/AuthToken.tsx
@@ -1,13 +1,9 @@
 type Token = string | null;
 
 export const getToken = (): Promise<Token> => {
-	return new Promise((resolve, reject) => {
+	return new Promise((resolve) => {
 		chrome.storage.local.get('token', (result: any) => {
-			if (result.token) {
-				resolve(result.token);
-			} else {
-				reject(new Error('Token not found in storage'));
-			}
+			resolve(result.token);
 		});
 	});
 };
