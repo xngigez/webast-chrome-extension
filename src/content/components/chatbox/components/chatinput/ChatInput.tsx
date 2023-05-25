68fa7950c6dc98c6c61e97cfe65255074139f2e2
Moses Mwangi <ngigemoses1@outlook.com>
2023-05-25 21:48:49 +0300

    Add function to handle chat input
    Co-Authored-By: Moses Mwangi <moses.soft.eng@gmail.com>

--- src/content/components/chatbox/components/chatinput/ChatInput.tsx
+++ src/content/components/chatbox/components/chatinput/ChatInput.tsx
@@ -1,29 +1,49 @@
-// TODO: add docs
-import './chatinput.scss';
+import './ChatInput.scss';
 import {SendIcon} from '../../assests/Icons';
 import React, {useState} from 'react';
 
-
-type ChatInput = {
-	onSend: (message: string) => string;
+/**
+ * Props for the ChatInput component.
+ */
+type ChatInputProps = {
+	sendMessage: (message: message) => void;
 }
 
+/**
+ * ChatInput component.
+ * @param {ChatInputProps} props - The props for the component.
+ * @returns {JSX.Element} The rendered component.
+ */
+export default function ChatInput({sendMessage}: ChatInputProps): JSX.Element {
+	const [inputMessage, setInputMessage] = useState<string>('');
 
-export default function ChatInput({onSend}: ChatInput): JSX.Element {
-	const [messsage, setMessage] = useState<string>('hi');
+	/**
+	 * Event handler for input change.
+	 * @param {React.ChangeEvent<HTMLTextAreaElement>} event - The change event.
+	 */
+	const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setInputMessage(event.target.value);
+	};
 
+	/**
+	 * Event handler for sending a message.
+	 */
+	const handleSendMessage = () => {
+		sendMessage({role: 'user', content: inputMessage});
+	};
 
 	return (
 		<div className="chat-input fixed-bottom-center">
 			<textarea
-				onChange={(event: React.ChangeEvent<HTMLTextAreaElement>): void => setMessage(event.target.value)}
-				value={messsage}
-				className="textarea" placeholder="Type your message..." />
+				onChange={handleInputChange}
+				value={inputMessage}
+				className="textarea" placeholder="Type your message..."
+			/>
 
 			<div className="icons">
 				<div className="icon"></div>
 
-				<div onClick={() => onSend(messsage)} className="icon">
+				<div onClick={handleSendMessage} className="icon">
 					<SendIcon />
 				</div>
 			</div>
